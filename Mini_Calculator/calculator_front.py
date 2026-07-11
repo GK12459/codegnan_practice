351aa40d3de819e9b01701176cf902c4d0368582
GK12459 <karthikeyagolusula92@gmail.com>
2026-07-11 12:50:25 +0530

    Modified for better error handling.

--- Mini_Calculator/calculator_front.py
+++ Mini_Calculator/calculator_front.py
@@ -11,32 +11,37 @@ while(True):
     if(operator not in operator_list):
         print("\nInvalid Operator!")
         continue
+    try:
+        n1 = float(input("Number 1: "))
+        n2 = float(input("Number 2: "))
+        match(operator):
+            case '+':
+                print("\nOperation__Addition")
+                print(calculator_back.sum_(n1, n2))
+            case '-':
+                print("\nOperation__Subtraction")
+                print(calculator_back.difference_(n1, n2))
 
-    n1 = int(input("Number 1: "))
-    n2 = int(input("Number 2: "))
-    match(operator):
-        case '+':
-            print("\nOperation__Addition")
-            print(calculator_back.sum_(n1, n2))
-        case '-':
-            print("\nOperation__Subtraction")
-            print(calculator_back.difference_(n1, n2))
+            case '*':
+                print("\nOperation__Multiplication")
+                print(calculator_back.product_(n1, n2))
+            
+            case '/':
+                print("\nOperation__Divission")
+                print(calculator_back.quotient_(n1, n2))
 
-        case '*':
-            print("\nOperation__Multiplication")
-            print(calculator_back.product_(n1, n2))
+            case '%':
+                print("\nOperation__Remainder")
+                print(calculator_back.modulus_(n1, n2))
 
-        case '/':
-            print("\nOperation__Divission")
-            print(calculator_back.quotient_(n1, n2))
-
-        case '%':
-            print("\nOperation__Remainder")
-            print(calculator_back.modulus_(n1, n2))
-
-        case '^':
-            print("\nOperation__Exponent")
-            print(calculator_back.exponent_(n1, n2))
+            case '^':
+                print("\nOperation__Exponent")
+                print(calculator_back.exponent_(n1, n2))
+    except(ValueError):
+        print("Invalid input!")
+    
+    except(ZeroDivisionError):
+        print("Cannot perform operation with 0!")
         
     try:
         input('Click enter to continue')
